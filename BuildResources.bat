c87b65eb1a247d89ef8d9f52c050423234f65bcc
SANE5K <84617085+aleksandrresetnikov@users.noreply.github.com>
2022-09-01 16:50:23 +0300

    Build events

---
+++ BuildResources.bat
@@ -0,0 +1,4 @@
+@echo off
+XCOPY ".\Source Resources\Themes\" ".\FreeIDE.Main\bin\Debug\Data\Themes\" /y /e
+XCOPY ".\Source Resources\Languages\" ".\FreeIDE.Main\bin\Debug\Data\Languages\" /y /e
+exit

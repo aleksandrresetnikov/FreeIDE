fc52c8f166f6d3cbd2ea3806171a2e6c6d97d0a0
SANE5K <84617085+aleksandrresetnikov@users.noreply.github.com>
2022-09-03 17:05:18 +0300

    Improvement

---
+++ FreeIDE.Main/Controls/SmartTextBox.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using FastColoredTextBoxNS;
+
+namespace FreeIDE.Controls
+{
+    internal class SmartTextBox : FastColoredTextBox
+    {
+
+    }
+}
